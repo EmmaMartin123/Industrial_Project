19fe1e4e00089a01260cf8dff0677a533fb7509f
VIKTORIIA0107 <2528502@dundee.ac.uk>
2025-09-24 17:17:08 +0100

    changed layout page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<Navbar />
+
 				{children}
+				
 				<Toaster position="top-right" reverseOrder={false} />
 
 				<Footer />
